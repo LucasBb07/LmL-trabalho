87a648da89c38c8b1a7c5f16089e912d2b82c063
LucasBb07 <barcelloslucas07@gmail.com>
2025-06-05 14:05:22 -0300

    pronto

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-const url = "mongodb+srv://luan:luan@classi.au203n7.mongodb.net/?retryWrites=true&w=majority&appName=Classi"
+
+const url = "mongodb+srv://aluno:aluno@cluster0.kgvfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const conexao = await mongoose.connect(url)
 export default mongoose
